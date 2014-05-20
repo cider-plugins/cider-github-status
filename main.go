c782cda0dc2a3f19dc4dc99b4c82ad3177f160dd
Ondrej Kupka <ondra.cap@gmail.com>
2014-05-20 10:11:28 +0200

    Rename Cider to Meeko, Paprika to Cider, refactor

--- main.go
+++ main.go
@@ -12,117 +12,88 @@ import (
 	"strings"
 	"syscall"
 
-	// Cider
-	"github.com/cider/go-cider/cider/services/logging"
-	"github.com/cider/go-cider/cider/services/pubsub"
-	zlogging "github.com/cider/go-cider/cider/transports/zmq3/logging"
-	zpubsub "github.com/cider/go-cider/cider/transports/zmq3/pubsub"
+	// Meeko
+	"github.com/meeko/go-meeko/agent"
+	"github.com/meeko/go-meeko/meeko/services/pubsub"
 
 	// Others
 	"code.google.com/p/goauth2/oauth"
 	"github.com/google/go-github/github"
-	zmq "github.com/pebbe/zmq3"
 )
 
 func main() {
-	// Initialise the Logging service.
-	logger, err := logging.NewService(func() (logging.Transport, error) {
-		factory := zlogging.NewTransportFactory()
-		factory.MustReadConfigFromEnv("CIDER_ZMQ3_LOGGING_").MustBeFullyConfigured()
-		return factory.NewTransport(os.Getenv("CIDER_ALIAS"))
-	})
-	if err != nil {
-		panic(err)
-	}
-	defer zmq.Term()
-	defer logger.Close()
+	// Make sure the Meeko agent is terminated properly.
+	defer agent.Terminate()
 
-	if err := innerMain(logger); err != nil {
-		panic(err)
+	// Run the main function.
+	if err := run(); err != nil {
+		os.Exit(1)
 	}
 }
 
-func innerMain(logger *logging.Service) error {
+func run() error {
+	// Some userful shortcuts.
+	var (
+		log      = agent.Logging
+		eventBus = agent.PubSub
+	)
+
 	// Read the required variables from the environment.
 	token := os.Getenv("GITHUB_TOKEN")
 	if token == "" {
-		return logger.Critical("GITHUB_TOKEN is not set")
+		return log.Critical("GITHUB_TOKEN is not set")
 	}
 
 	// Initialise the GitHub client.
-	transport := oauth.Transport{
+	t := oauth.Transport{
 		Token: &oauth.Token{AccessToken: token},
 	}
-	client := github.NewClient(transport.Client())
-
-	// Initialise the PubSub service.
-	eventBus, err := pubsub.NewService(func() (pubsub.Transport, error) {
-		factory := zpubsub.NewTransportFactory()
-		factory.MustReadConfigFromEnv("CIDER_ZMQ3_PUBSUB_").MustBeFullyConfigured()
-		return factory.NewTransport(os.Getenv("CIDER_ALIAS"))
-	})
-	if err != nil {
-		return logger.Critical(err)
-	}
-	defer func() {
-		select {
-		case <-eventBus.Closed():
-			goto Wait
-		default:
-		}
-		if err := eventBus.Close(); err != nil {
-			logger.Critical(err)
-		}
-	Wait:
-		if err := eventBus.Wait(); err != nil {
-			logger.Critical(err)
-		}
-	}()
+	gh := github.NewClient(t.Client())
 
 	// Start catching signals.
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(signalCh, syscall.SIGINT, syscall.SIGTERM)
 
 	// Subscribe to all significant build events.
-	if _, err := eventBus.Subscribe("paprika.build.enqueued", func(event pubsub.Event) {
+	if _, err := eventBus.Subscribe("cider.build.enqueued", func(event pubsub.Event) {
 		// Unmarshal the event object.
 		var body BuildEnqueuedEvent
 		if err := event.Unmarshal(&body); err != nil {
-			logger.Warn(err)
+			log.Warn(err)
 			return
 		}
 
 		// Do nothing if the build source is not a pull request.
 		if body.PullRequest == nil {
-			logger.Info("ENQUEUED event received, but not a pull request, skipping...")
+			log.Info("ENQUEUED event received, but not a pull request, skipping...")
 			return
 		}
 
 		// Set the pull request status to pending.
-		logger.Infof("Setting status for %v to PENDING", body.PullRequest.HTMLURL)
-		if err := postStatus(client, body.PullRequest.StatusesURL, &github.RepoStatus{
+		log.Infof("Setting status for %v to PENDING", body.PullRequest.HTMLURL)
+		if err := postStatus(gh, body.PullRequest.StatusesURL, &github.RepoStatus{
 			State:       github.String("pending"),
 			Description: github.String("The build is enqueued or running"),
-			Context:     github.String("Paprika CI"),
+			Context:     github.String("Cider CI"),
 		}); err != nil {
-			logger.Error(err)
+			log.Error(err)
 			return
 		}
 	}); err != nil {
-		return logger.Critical(err)
+		return log.Critical(err)
 	}
 
-	if _, err := eventBus.Subscribe("paprika.build.finished", func(event pubsub.Event) {
+	if _, err := eventBus.Subscribe("cider.build.finished", func(event pubsub.Event) {
 		// Unmarshal the event object.
 		var body BuildFinishedEvent
 		if err := event.Unmarshal(&body); err != nil {
-			logger.Warn(err)
+			log.Warn(err)
 			return
 		}
 
 		// Do nothing if the build source is not a pull request.
 		if body.PullRequest == nil {
-			logger.Info("FINISHED event received, but not a pull request, skipping...")
+			log.Info("FINISHED event received, but not a pull request, skipping...")
 			return
 		}
 
@@ -130,37 +101,36 @@ func innerMain(logger *logging.Service) error {
 		var desc string
 		switch body.Result {
 		case "success":
-			desc = "The build succeeded!"
+			desc = "Cider build succeeded!"
 		case "failure":
-			desc = "The build failed!"
+			desc = "Cider build failed!"
 		case "error":
 			if body.Error != "" {
 				desc = body.Error
 			} else {
-				desc = "Paprika exploded, oops"
+				desc = "Cider exploded, oops"
 			}
 		}
 
-		logger.Infof("Setting status for %v to %v",
+		log.Infof("Setting status for %v to %v",
 			body.PullRequest.HTMLURL, strings.ToUpper(body.Result))
-		if err := postStatus(client, body.PullRequest.StatusesURL, &github.RepoStatus{
+		if err := postStatus(gh, body.PullRequest.StatusesURL, &github.RepoStatus{
 			State:       &body.Result,
 			TargetURL:   body.OutputURL,
 			Description: &desc,
-			Context:     github.String("Paprika CI"),
+			Context:     github.String("Cider"),
 		}); err != nil {
-			logger.Error(err)
+			log.Error(err)
 			return
 		}
 	}); err != nil {
-		return logger.Critical(err)
+		return log.Critical(err)
 	}
 
 	// Start processing signals, block until crashed or terminated.
 	select {
-	case <-eventBus.Closed():
-		return eventBus.Wait()
 	case <-signalCh:
+		log.Info("Signal received, exiting...")
 		return nil
 	}
 }
